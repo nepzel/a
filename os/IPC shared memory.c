4a7787802fae18523cb769754d317cbef30c031b
Nepzel <109920453+nepzel@users.noreply.github.com>
2022-07-27 17:32:38 +0530

    Add files via upload

---
+++ os/IPC shared memory.c
@@ -0,0 +1,36 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+int writer()
+{
+key_t key = ftok("shmfile",65);
+int shmid = shmget(key,1024,0666|IPC_CREAT);
+char *str = (char*) shmat(shmid,(void*)0,0);
+printf("Data to write : ");
+gets(str);
+shmdt(str);
+return 0;
+}
+int reader(){
+key_t key = ftok("shmfile",65);
+int shmid = shmget(key,1024,0666|IPC_CREAT);
+char *str = (char*) shmat(shmid,(void*)0,0);
+printf("Data read from memory: %s\n",str);
+shmdt(str);
+shmctl(shmid,IPC_RMID,NULL);
+return 0;
+}
+void main(){
+pid_t child=fork();
+if(child==0){
+writer();
+}
+else if(child>0){
+sleep(10);
+reader();
+exit(0);
+}
+}
